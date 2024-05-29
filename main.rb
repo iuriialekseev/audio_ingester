79e8ce19af8c06031b582f5e9ed00539097c592c
Iurii Alekseev <iurii.alekseev.inbox@gmail.com>
2024-05-29 12:04:27 +0200

    Remove unnecessary condition in main.rb

--- main.rb
+++ main.rb
@@ -2,17 +2,15 @@
 
 require_relative 'lib/processor'
 
-if __FILE__ == $PROGRAM_NAME
-  if ARGV.length != 1
-    puts 'Usage: bundle exec ruby main.rb <input_dir>'
-    exit 1
-  end
-
-  input_dir = ARGV[0]
-  unless Dir.exist?(input_dir)
-    puts "The directory #{input_dir} does not exist."
-    exit 1
-  end
-
-  Processor.process(input_dir)
+if ARGV.length != 1
+  puts 'Usage: bundle exec ruby main.rb <input_dir>'
+  exit 1
 end
+
+input_dir = ARGV[0]
+unless Dir.exist?(input_dir)
+  puts "The directory #{input_dir} does not exist."
+  exit 1
+end
+
+Processor.process(input_dir)
